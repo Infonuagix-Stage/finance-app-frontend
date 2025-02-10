5b4678db8e6c03c9c711e3b8fc654f608b81ae9f
Guillaume <guillaumefournier201@gmail.com>
2025-02-10 15:18:55 -0500

    Configuring navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,4 @@
-// App.js
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Signup";
 import Dashboard from "./components/LoadingPage";
@@ -12,29 +6,47 @@ import PrivateRoute from "./components/PrivateRoute";
 import BudgetingPage from "./components/BudgetingPage";
 import CategoryPage from "./components/CategoryPage";
 import DashboardPage from "./components/Dashboard";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider, useAuthContext } from "./context/AuthContext";
+import LoadingPage from "./components/LoadingPage";
+import About from "./components/About";
+import Project from "./components/Project";
+import Navbar from "./components/Navbar";
+import Payment from "./components/Payment";
+
+const AppContent = () => {
+  const { user } = useAuthContext(); // Vérifie si l'utilisateur est connecté
+
+  return (
+    <div className="pt-16">
+      {user && <Navbar />} 
+      <Routes>
+        {/* Routes publiques */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+
+        {/* Routes protégées */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/loadingpage" element={<Dashboard />} />
+          <Route path="/budgeting" element={<BudgetingPage />} />
+          <Route path="/category/:categoryName" element={<CategoryPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/payment" element={<Payment/>} />
+        </Route>
+
+        {/* Route de fallback */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </div>
+  );
+};
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          {/* Routes publiques */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Register />} />
-
-          {/* Routes protégées */}
-          <Route element={<PrivateRoute />}>
-            <Route path="/loadingpage" element={<Dashboard />} />
-            <Route path="/budgeting" element={<BudgetingPage />} />
-            {/* On passe l'ID de la catégorie */}
-            <Route path="/category/:categoryName" element={<CategoryPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
-
-          {/* Route de fallback */}
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
+        <AppContent />
       </Router>
     </AuthProvider>
   );
