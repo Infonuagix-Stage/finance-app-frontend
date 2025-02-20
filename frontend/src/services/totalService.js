798f8bb4d55891bf37e2b0d2b23f2f36fabee799
Guillaume <guillaumefournier201@gmail.com>
2025-02-20 10:18:31 -0500

    base url need to be fixed

--- frontend/src/services/totalService.js
+++ frontend/src/services/totalService.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://finance-app-frontend-8bmb.onrender.com/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
