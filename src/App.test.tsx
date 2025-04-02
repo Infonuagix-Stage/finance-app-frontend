6472c042a1b8715ab475bf7669f3fc02b86829f4
Mariya Zolovska <2230965@champlaincollege.qc.ca>
2025-04-02 14:50:13 -0400

    fixed conflicts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,11 @@
-import { render } from "@testing-library/react";
-import { screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom"; // Use BrowserRouter or any other Router
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders login page", () => {
-  render(
-    <BrowserRouter> {/* Use BrowserRouter instead of MemoryRouter */}
-      <App />
-    </BrowserRouter>
-  );
-
-  // Look for the Login page heading or button
-  const loginHeading = screen.getByRole("heading", { name: /login/i });
-  expect(loginHeading).toBeInTheDocument();
+test('renders login page', () => {
+  const { getByText } = render(<App />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const loginElement = getByText(/login/i);
+  expect(loginElement).toBeInTheDocument();
 });
+
