11f24a9c6e9915e27435a56ce66d4d0c96b4ba9f
Mariya Zolovska <2230965@champlaincollege.qc.ca>
2025-04-02 14:45:32 -0400

    fixed conflicts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,13 @@
 import { render } from "@testing-library/react";
-import { screen } from "@testing-library/dom";
+import { screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
 test("renders login page", () => {
   render(
+    <MemoryRouter initialEntries={["/login"]}>
       <App />
+    </MemoryRouter>
   );
 
   // Look for the Login page heading or button
